29bcfe322adfc1786d67d543f42d0bf773c9c14d
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-25 00:08:23 -0400

    Update VerifyToken.js

--- models/VerifyToken.js
+++ models/VerifyToken.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const verifyTokenSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   code: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 600 } // expires in 10 mins
+  resendAttempts: { type: Number, default: 0 }, // NEW
+  createdAt: { type: Date, default: Date.now, expires: 600 } // expires in 10 minutes
 });
 
 module.exports = mongoose.model('VerifyToken', verifyTokenSchema);
