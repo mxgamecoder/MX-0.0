3a927dab10c87eeaeafe37fe5173a4da74d3ddc7
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-27 03:43:29 -0400

    Create plan.js

---
+++ routes/plan.js
@@ -0,0 +1,45 @@
+// plans.js
+module.exports = {
+  free: {
+    name: "Free Plan",
+    limit: 800,
+    storage: 512,
+    coins: 0,
+    price: 0
+  },
+  basic: {
+    name: "Basic Plan",
+    limit: 2000,
+    storage: 1024,
+    coins: 200,
+    price: 2
+  },
+  starter: {
+    name: "Starter Plan",
+    limit: 5000,
+    storage: 2048,
+    coins: 500,
+    price: 5
+  },
+  premium: {
+    name: "Premium Plan",
+    limit: 10000,
+    storage: 5000,
+    coins: 1000,
+    price: 10
+  },
+  pro: {
+    name: "Pro Plan",
+    limit: 50000,
+    storage: 20000,
+    coins: 5000,
+    price: 25
+  },
+  elite: {
+    name: "Elite Plan",
+    limit: 100000,
+    storage: 100000,
+    coins: 10000,
+    price: 50
+  }
+};
