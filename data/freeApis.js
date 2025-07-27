8508af5b3c62deeb58039f5d151076e3e77e2740
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-27 09:01:57 -0400

    Update freeApis.js

--- data/freeApis.js
+++ data/freeApis.js
@@ -1,8 +1,18 @@
 module.exports = [
+  // Fun
   "fun/joke",
   "fun/dare",
   "fun/fact",
   "fun/compliment",
   "fun/truth",
-  "fun/pickup"
+  "fun/pickup",
+
+  // Jest
+  "jest/8ball",
+  "jest/avatar",
+  "jest/cuddle",
+
+  // NSFW
+  "nsfw/blowjob",
+  "nsfw/neko",
 ];
