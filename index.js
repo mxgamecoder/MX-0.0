3136939023e68cc7fc52a216968215f9e04de80f
msgamecoder <olamilekandamilaraa@gmail.com>
2025-08-29 06:27:16 -0700

    lumora

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 const allowedOrigins = [
-  'https://mxgamecoder-klfx.onrender.com',
+  'https://mxapi-lnc.onrender.com',
   'https://f1277a31-82ea-4b2c-9e22-fa1e4197a39d-00-1falu0he0oln1.riker.replit.dev',
 ];
 
