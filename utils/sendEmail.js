9ec57f6f86408017b19b9ae5712a14952561e678
MX-GΔMΞCØDΞR <olamilekandamilaraa@gmail.com>
2025-09-01 14:30:55 -0700

    m

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
