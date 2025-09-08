63a2bf5500a0638947f127114afe7e88ad550a8f
MX-GΔMΞCØDΞR <olamilekandamilaraa@gmail.com>
2025-09-08 11:53:46 -0700

    Update VerifyToken.js

--- models/VerifyToken.js
+++ models/VerifyToken.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const verifyTokenSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   code: { type: String, required: true },
+  purpose: { type: String, enum: ["register", "reset", "update"], default: "register" },
   resendAttempts: { type: Number, default: 0 }, // NEW
   createdAt: { type: Date, default: Date.now, expires: 600 } // expires in 10 minutes
 });
