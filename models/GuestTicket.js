c5e704ce5595f8f135e938564a436cad956eed44
MX-GΔMΞCØDΞR <olamilekandamilaraa@gmail.com>
2025-09-20 13:58:10 -0700

    Update GuestTicket.js

--- models/GuestTicket.js
+++ models/GuestTicket.js
@@ -1,12 +1,23 @@
+// models/GuestTicket.js
 const mongoose = require("mongoose");
 
-const GuestTicketSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true },
-  attachments: [String],
-  status: { type: String, default: "received" },
-  createdAt: { type: Date, default: Date.now }
-});
+const GuestTicketSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+    attachments: [String],
+    status: { type: String, default: "received" },
+    replies: [
+      {
+        username: String,
+        message: String,
+        createdAt: { type: Date, default: Date.now },
+        isAdmin: { type: Boolean, default: false },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("GuestTicket", GuestTicketSchema);
