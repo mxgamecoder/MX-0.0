e6583f9af767bfc5b89a14252498274042ba4765
MX-GΔMΞCØDΞR <olamilekandamilaraa@gmail.com>
2025-09-21 08:43:07 -0700

    Create packages.js

---
+++ utils/packages.js
@@ -0,0 +1,100 @@
+// config/packages.js
+
+module.exports = [
+  {
+    id: "pack1",
+    coins: 10,
+    ngn: 100,
+    usd: 0.25,
+    eur: 0.23,
+    ghs: 3
+  },
+  {
+    id: "pack2",
+    coins: 50,
+    ngn: 500,
+    usd: 1.25,
+    eur: 1.15,
+    ghs: 15
+  },
+  {
+    id: "pack3",
+    coins: 100,
+    ngn: 1000,
+    usd: 2.5,
+    eur: 2.3,
+    ghs: 30
+  },
+  {
+    id: "pack4",
+    coins: 500,
+    ngn: 5000,
+    usd: 12.5,
+    eur: 11.5,
+    ghs: 150
+  },
+  {
+    id: "pack5",
+    coins: 1000,
+    ngn: 10000,
+    usd: 25,
+    eur: 23,
+    ghs: 300
+  },
+  {
+    id: "pack6",
+    coins: 5000,
+    ngn: 50000,
+    usd: 125,
+    eur: 115,
+    ghs: 1500
+  },
+  {
+    id: "pack7",
+    coins: 10000,
+    ngn: 100000,
+    usd: 250,
+    eur: 230,
+    ghs: 3000
+  },
+  {
+    id: "pack8",
+    coins: 50000,
+    ngn: 500000,
+    usd: 1250,
+    eur: 1150,
+    ghs: 15000
+  },
+  {
+    id: "pack9",
+    coins: 100000,
+    ngn: 1000000,
+    usd: 2500,
+    eur: 2300,
+    ghs: 30000
+  },
+  {
+    id: "pack10",
+    coins: 500000,
+    ngn: 5000000,
+    usd: 12500,
+    eur: 11500,
+    ghs: 150000
+  },
+  {
+    id: "pack11",
+    coins: 1000000,
+    ngn: 10000000,
+    usd: 25000,
+    eur: 23000,
+    ghs: 300000
+  },
+  {
+    id: "pack12",
+    coins: 5000000,
+    ngn: 50000000,
+    usd: 125000,
+    eur: 115000,
+    ghs: 1500000
+  }
+];
